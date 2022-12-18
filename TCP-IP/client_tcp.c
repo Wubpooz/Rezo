2091244615733c4f6d2eb600c4e617fbf3be0311
Wubpooz <mathieu.waharte@gmail.com>
2022-12-19 00:33:05 +0100

    start tcp

---
+++ TCP-IP/client_tcp.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <limits.h>
+
+#define SIZE 100
+#define PORT 9600
+
+
+int main(int argc, char* argv[]){
+
+    int sockfd;
+    struct sockaddr_in server;
+    struct hostent *host;
+    char buff[SIZE];
+    unsigned int len = 20;
+
+
+    if((sockfd = socket(PF_INET,SOCK_DGRAM,0))<0){ 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    if(argc<2){return 1;}
+    host = gethostbyname(argv[1]);
+    if(!host) return 1;
+    char* IP = inet_ntoa(*(struct in_addr*)host->h_addr);
+
+    server.sin_family = host->h_addrtype;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr(IP);
+
+
+    printf("Client message : ");
+    fgets(buff, len, stdin);    // = scanf("%lens",buff);
+    sendto(sockfd,buff,SIZE,0,(const struct sockaddr *) &server,sizeof(server));
+    printf("msg sent : %s\n",buff);
+    printf("Message sent.\n");
+
+    close(sockfd);
+    return 0;
+}
---
+++ TCP-IP/server_tcp.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT 9600
+
+
+int main(int argc, char* argv[]){
+
+    int sockfd;
+    struct sockaddr_in client, server;
+    unsigned int size = sizeof(client);
+
+    
+    if((sockfd = socket(PF_INET,SOCK_DGRAM,0))<0){ 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+    
+    if(bind(sockfd,(const struct sockaddr *)&server,sizeof(server))<0){ 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+
+    char* msg_rec = malloc(20);
+    int rec_len = 0;
+
+    while(1){
+        rec_len = recvfrom(sockfd,msg_rec,size,0,(struct sockaddr *)&client,&size);
+        if(msg_rec!=NULL && rec_len!=-1){break; close(sockfd);}
+    }
+    
+    msg_rec[rec_len] = '\0';
+    printf("Message : %s\n",msg_rec);
+
+    return 0;
+}
