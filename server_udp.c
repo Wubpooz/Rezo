96ace2b43879b15f94fa6f432f93e314a8193785
Wubpooz <mathieu.waharte@gmail.com>
2022-12-18 21:06:47 +0100

    bind not working ?

--- server_udp.c
+++ server_udp.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]){
     int sockfd;
     struct sockaddr_in client;
     struct sockaddr_in server;
-    unsigned int size = 100;    // right value ?    supposed to be client address size
+    unsigned int size = 20;    // right value ?    supposed to be client address size
 
     
     sockfd = socket(PF_INET, SOCK_DGRAM,0);
@@ -23,17 +23,26 @@ int main(int argc, char* argv[]){
     client.sin_port = PORT;
     client.sin_addr.s_addr = INADDR_ANY;
     
-    server.sin_port = bind(sockfd,(struct sockaddr *)&client,size); // sin port still 0 after that
+    bind(sockfd,(struct sockaddr *)&server,sizeof(server)); // sin port still 0 after that
  
+    // if (bind(sockfd,(struct sockaddr *)&server,sizeof(server)) == 0) {
+    //     fprintf(stdout,"bind completed\n");
+    // }
+    // else {
+    //     perror("Could not bind to socket");
+    //     close(sockfd);
+    //     exit(1);
+    // }
  
     char* msg_rec = '\0';
 
     while(1){
         recvfrom(sockfd,msg_rec,size,0,(struct sockaddr *)&client,&size);
-        printf("%s\n",msg_rec);
 
-        if(*msg_rec!='\0'){close(sockfd);}
+        if(*msg_rec!='\0'){break; close(sockfd);}
     }
+    
+    printf("Message reçu : %s\n",msg_rec);
 
     return 0;
 }
